7e8eeff30b2b8e41305cc868d046e5e5cb21f711
ChristinaGaitan <christina.gaitan@gmail.com>
2021-03-25 13:06:06 -0600

    Rename reducer file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import reducer from './store/store'
+import reducer from './store/reducer'
 
 const store = createStore(reducer)
 
